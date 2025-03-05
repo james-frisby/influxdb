dd351d1a1530cba0063d980b7aa04ec4df69590d
James Frisby <frisbyjamesp@gmail.com>
2025-03-04 23:28:22 -0600

    Random walk

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # influxdb
+
+## setup docs
+https://docs.influxdata.com/influxdb/v2/get-started/setup/#run-initial-setup-process
+
+## docker command to install + run influxdb
+```docker run --detach --name influxdb2 --publish 8086:8086 --mount type=volume,source=influxdb2-data,target=/var/lib/influxdb2 --mount type=volume,source=influxdb2-config,target=/etc/influxdb2 --env DOCKER_INFLUXDB_INIT_MODE=setup --env DOCKER_INFLUXDB_INIT_USERNAME=admin --env DOCKER_INFLUXDB_INIT_PASSWORD=adminadmin --env DOCKER_INFLUXDB_INIT_ORG=james-org --env DOCKER_INFLUXDB_INIT_BUCKET=james-bucket influxdb:2```
+
+
+## influxdb localhost
+
+```http://localhost:8086/```
+
---
+++ src/random_walk/RandomWalk/Program.cs
@@ -0,0 +1,57 @@
+﻿using InfluxDB.Client;
+using InfluxDB.Client.Core;
+using InfluxDB.Client.Writes;
+
+namespace RandomWalk;
+
+class Program
+{
+    private static readonly string TokenFilePath = ".influx-token";
+    private static readonly string JamesBucket = "james-bucket";
+    private static readonly string HelloRecordBucket = "helloRecord";
+    private static readonly string Organization = "james-org";
+    private static readonly string HelloMeasurement = "helloMeasurement";
+    private static readonly string RandomWalkMeasurement = "RandomWalkMeasurement";
+    private static WriteApi WriteApi = null;
+
+    static async Task Main(string[] args)
+    {
+
+        var token = GetInfluxDBToken();
+        using var client = new InfluxDBClient("http://localhost:8086", token);
+        using var writeApi = client.GetWriteApi();
+        WriteApi = writeApi;
+
+        double val = 1000;
+        while (true)
+        {
+            Measure(val);
+            await Task.Delay(5);
+            var delta = Random.Shared.NextDouble() - Random.Shared.NextDouble();
+            val -= delta;
+        }
+    }
+
+    static void Measure(double val)
+    {
+        WriteApi.WritePoint(
+            PointData.Measurement(RandomWalkMeasurement)
+                .Field("value", val)
+                .Tag("user", "james")
+                .Timestamp(DateTime.Now, InfluxDB.Client.Api.Domain.WritePrecision.Ms),
+            JamesBucket,
+            Organization
+        );
+    }
+
+    static string GetInfluxDBToken()
+    {
+        return File.ReadAllText(TokenFilePath);
+    }
+
+    [Measurement("hellopoco")]
+    private class HelloPoco
+    {
+
+    }
+}
